04852abf11060c753c0cc9a556af67ab0a79244f
Trung Dinh Quang <trungdq88@gmail.com>
2016-11-21 15:52:03 +0700

    Add command

--- index.js
+++ index.js
@@ -3,16 +3,39 @@ const log = (...args) => window.name === 'TRUNGDQ_DEBUG' && console.log.call(con
 // notation => function
 const COMMANDS = {
   'C-u': () => {
-    window.scrollBy(0, -window.innerHeight/2)
+    window.scrollBy(0, -window.innerHeight/2);
   },
   'C-d': () => {
-    window.scrollBy(0, window.innerHeight/2)
+    window.scrollBy(0, window.innerHeight/2);
+  },
+  'gg': () => {
+    window.scrollTo(0, 0);
+  },
+  'G': () => {
+    window.scrollTo(0, Number.MAX_SAFE_INTEGER)
   },
 }
+
 const keyStack = [];
 
 function composeNotation(metaKey, key) {
-  return metaKey + '-' + key;
+  return metaKey + key;
+}
+
+function showIncompleteCommand(notation) {
+  let element = document.getElementById('chrome-vim');
+  if (!element) {
+    document.body.insertAdjacentHTML('beforeend', `
+      <div id="chrome-vim" style="position: fixed; background: yellow; bottom: 0; right: 0"></div>
+    `);
+    element = document.getElementById('chrome-vim');
+  }
+  element.innerText = notation;
+}
+
+function hideIncompleteCommand() {
+  let element = document.getElementById('chrome-vim');
+  element && element.parentNode.removeChild(element);
 }
 
 function execute(notation) {
@@ -27,15 +50,20 @@ function match(notation) {
   log(find);
 
   switch (find.length) {
-      case 0: return 'not_found';
-      case 1: return 'found';
-      default: return 'found_multiple_commands';
+    case 0: return 'not_found';
+    case 1:
+      if (find[0].length === notation.length) {
+        return 'found';
+      } else {
+        return 'found_but_not_complete';
+      }
+    default: return 'found_multiple_commands';
   }
 }
 
 window.addEventListener('keydown', e => {
   log(e);
-  const metaKey = e.ctrlKey ? 'C' : '';
+  const metaKey = e.ctrlKey ? 'C-' : '';
   const key = e.key;
   keyStack.push(key);
 
@@ -43,14 +71,19 @@ window.addEventListener('keydown', e => {
   const check = match(notation);
   log(notation);
   log(check);
-  if (check === 'found_multiple_commands') {
+  if (check === 'found_multiple_commands' || check === 'found_but_not_complete') {
     // wait for further keys
+    showIncompleteCommand(notation);
   } else if (check === 'found') {
     execute(notation);
     keyStack.length = 0;
+    hideIncompleteCommand();
   } else if (check === 'not_found') {
     keyStack.length = 0;
+    hideIncompleteCommand();
   } else {
     console.error('[chrome-vim] Developer sucks! check: ', check);
   }
 }, false);
+
+log('Loaded.');
